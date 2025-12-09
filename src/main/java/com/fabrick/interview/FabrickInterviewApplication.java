d59b48a364ff03215bad8da9d6fa93d9e38fd3df
Antonio Chiumiento <a.chiumiento@bitify.it>
2025-12-09 17:11:09 +0100

    chore: init maven project with reactive stack and caching

---
+++ src/main/java/com/fabrick/interview/FabrickInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.fabrick.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrick/interview/FabrickInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrick.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
