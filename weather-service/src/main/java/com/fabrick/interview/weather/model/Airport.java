a6541e0192b0c4157aaf46eb3a9281079567181f
Antonio Chiumiento <a.chiumiento@bitify.it>
2025-12-10 11:23:35 +0100

    chore: setup multi-module project structure and gitignore

---
+++ weather-service/src/main/java/com/fabrick/interview/weather/model/Airport.java
@@ -0,0 +1,28 @@
+package com.fabrick.interview.weather.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Airport {
+    private final String id;
+    private final String name;
+    private final float latitude;
+    private final float longitude;
+
+    public Airport(
+            @JsonProperty("station_id") String id,
+            @JsonProperty("site") String name,
+            @JsonProperty("latitude") float latitude,
+            @JsonProperty("longitude") float longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public float getLatitude() { return latitude; }
+    public float getLongitude() { return longitude; }
+}
